73b3b7b702aaf1c5fc0edbc05c102bc9d9f3dfe8
Nicolas <ncorantehuenchuman@gmail.com>
2024-09-10 11:41:05 -0300

    Creacion y modificacion de card pokemones

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
-<script setup>
+<script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon.vue';
+export default {
+  data() {
+    return {
+      pokemonsUrl: [],
+      pokemones: []
+    }
+  },
+  methods: {
+    async getPokemons() {
+      const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+      this.pokemonsUrl = data.results.map(poke => poke.url);
+      const pokePromise = this.pokemonsUrl.map(url => axios.get(url))
+      const responses = await Promise.all(pokePromise);
+      this.pokemones = responses.map(response => {
+        const pokemonData = response.data;
+        return {
+          name: pokemonData.name,
+          image: pokemonData.sprites.front_default,
+        };
+      })
+    },
+  },
+  mounted() {
+    this.getPokemons()
+  },
+  components: {
+    Pokemon
+  }
+}
 </script>
 
 <template>
+  <Pokemon :pokemones="pokemones"/>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card">
+        <div class="pokemon" v-for="pokemon of pokemones">
+            <img :src="pokemon.image" alt="">
+            <input type="text"><br>
+            <button>Descubrir</button>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        pokemones: Object
+    }
+}
+</script>
+<style scoped>
+.card {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+}
+img{
+   margin: 0 auto;
+   width: 100%;
+}
+.pokemon{
+    width: 100px;
+    margin: 0 auto;
+    padding: 10px;
+    text-align: center;
+}
+input{
+    width: 100%;
+    margin-bottom: 10px;
+    padding: 0;
+}
+</style>
